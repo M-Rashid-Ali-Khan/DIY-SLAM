a26fb4d793da5eb666da078966f658c378072fc1
Rashid Ali <rashid.ali@cowlar.com>
2024-08-18 18:08:42 +0500

    Fix: Window name

--- simulation/env.py
+++ simulation/env.py
@@ -4,12 +4,12 @@ import math
 import pygame
 
 class buildEnvironment:
-    def __init__(self,map_template,MapDimensionsWH):
+    def __init__(self,map_template,mapWindowName,MapDimensionsWH):
         pygame.init()
         self.pointCloud=[]
         self.externalMap = pygame.image.load(map_template)
         self.mapW,self.mapH = MapDimensionsWH
-        self.mapWindowName = 'SLAM PLanning'
+        self.mapWindowName = mapWindowName
         pygame.display.set_caption(self.mapWindowName)
         self.map = pygame.display.set_mode((self.mapW,self.mapH))
         self.map.blit(self.externalMap,(0,0))
@@ -19,4 +19,17 @@ class buildEnvironment:
         self.green = (0,255,0)
         self.red = (255,0,0)
         self.white = (255,255,255)
+    
+    def dataStorage(self,data:tuple[(any,any)]|None):
+        if data == None:
+            return
+        for point in data:
+            if point not in self.pointCloud:
+                self.pointCloud.append(point)
+
+    def show_sensorData(self):
+        self.infomap = self.map.copy()
+        for point in self.pointCloud:
+            self.infomap.set_at(point,self.red)
+
 
